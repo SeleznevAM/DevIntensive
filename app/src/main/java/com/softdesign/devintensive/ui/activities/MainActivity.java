a66bbd44c9806df026f7ac11fe760ceb0ff93c9c
Seleznev Alex <Seleznevam84@ya.ru>
2016-06-28 17:03:54 +0300

    lesson#3 practice

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,21 +1,27 @@
 package com.softdesign.devintensive.ui.activities;
 
 import android.graphics.Color;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
     protected EditText mEditText;
     protected Button mRedBtn, mGreenBtn;
     protected int mColorMode;
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
 
 
     @Override
@@ -23,12 +29,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate");
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container );
+        mCallImg.setOnClickListener(this);
 
 
         if (savedInstanceState==null){
             //активити было запущено впервые
+            showSnackBar("активити запущена впервые");
         }else{
             //активити уже создавалось
+            showSnackBar("активити уже создавалась");
 
             mColorMode = savedInstanceState.getInt(ConstantManager.COLOR_MODE_KEY);
             if(mColorMode==Color.RED){
@@ -80,6 +91,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.call_img:
+                showProgress();
+                runWithDelay();
+                break;
+        }
         Log.d(TAG, "OnClick");
     }
 
@@ -89,5 +106,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Log.d(TAG, "onSaveInstance");
         outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode);
+    }
+
+    private void runWithDelay(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        }, 5000);
+
+    }
+
+    private void showSnackBar(String message){
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
     }
 }
